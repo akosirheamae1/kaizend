0a442dc549817922bf0e039a5e063f76e11e098b
akosirheamae1 <58824346+akosirheamae1@users.noreply.github.com>
2019-12-13 06:44:27 -0800

    Update Exercise6.py

--- Exercise6.py
+++ Exercise6.py
@@ -1,7 +1,11 @@
-class Car:
+words = ["PoGo", "Spange", "Lie-Fi"]
+cooldictionary = {}
+definitions = [
+    "Slang for Pokemon Go",
+    "To collect spare change, either from couches, passerbys on the street or any numerous other ways and means",
+    "When your phone or tablet indicates that you are connected to a wireless network, however you are still unable to load webpages or use any internet services with your device"
+]
 
-    def __init__(self,year, make, model):
-        self.year = 2019-year
-        self.make = make
-        self.model = model
-Car(1994, 'make', 'Toyota').year
+for key, item in enumerate(words):
+    cooldictionary[item] = definitions[key]
+print(cooldictionary)
